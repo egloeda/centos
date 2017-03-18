ebee86e11e3d312739cc0e6b29eea381f0f24d6d
Emilio Gª Loeda <emilio.loeda@gmail.com>
2017-03-18 23:36:29 +0100

    Update php56.sh

--- php56.sh
+++ php56.sh
@@ -7,7 +7,7 @@ rpm -Uvh remi-release-7*.rpm
 sleep 3
 yum-config-manager --enable remi-php56
 sleep 3
-yum -y install httpd httpd-tools
+yum -y install httpd httpd-tools httpd-devel
 yum -y install php56 php56-php php56-php-devel php-pear php-devel
 
 service httpd start
