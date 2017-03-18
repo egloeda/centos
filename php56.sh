060d9ed9c8a04524d33351de2dfdf4e10c656053
Emilio Gª Loeda <emilio.loeda@gmail.com>
2017-03-18 15:03:57 +0100

    Update php56.sh

--- php56.sh
+++ php56.sh
@@ -2,9 +2,12 @@
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/egloeda/centos/master/php56.sh)"
 cd /root
 wget http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+sleep 3
 rpm -Uvh remi-release-7*.rpm
-yum -y install httpd httpd-tools
+sleep 3
 yum-config-manager --enable remi-php56
+sleep 3
+yum -y install httpd httpd-tools
 yum -y install php56 php56-php php56-php-devel
 
 service httpd start
